bdacb3a8c163737aafb5c6c6f306ee7a1f9e7f55
Ubuntu <ubuntu@ip-172-31-45-185.ap-southeast-2.compute.internal>
2024-06-08 03:24:15 +0000

    created the dockerfile and terraform

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+resource "aws_ecs_cluster" "main" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = var.app_name
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = var.app_name
+    image     = "shubham1361/hello-world-nodejs:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "main" {
+  name            = var.app_name
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-0123456789abcdef0"]
+    security_groups = ["sg-0123456789abcdef0"]
+  }
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "app_name" {
+  default = "hello-world-nodejs"
+}
+
+variable "ecs_cluster_name" {
+  default = "hello-world-cluster"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_profile" {
+  default = "default"
+}
+
