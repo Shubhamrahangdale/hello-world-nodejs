536d5f1040f21da10245940ac91c33f608f6738c
Shubham Rahangdale <102543960+Shubhamrahangdale@users.noreply.github.com>
2024-06-09 00:49:44 +0530

    Update outputs.tf
    update the ec2 service name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
 output "ecs_cluster_id" {
-  value = aws_ecs_cluster.main.id
+  value = aws_ecs_cluster.helloNodejs.id
 }
 
 output "ecs_service_name" {
-  value = aws_ecs_service.main.name
+  value = aws_ecs_service.helloNodejs.name
 }
 
